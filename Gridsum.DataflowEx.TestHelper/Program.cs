378d9eeb4b627f645278439122321ca92512af9b
karldodd <karldodd@gmail.com>
2014-08-19 14:49:10 +0800

    Add TestHelper to help testing (VS test explorer doesn't show log of running/hanging tests)

---
+++ Gridsum.DataflowEx.TestHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gridsum.DataflowEx.TestHelper
+{
+    using Gridsum.DataflowEx.Test.ETL;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TestDataJoiner().TestDataJoinerJoining().Wait();
+        }
+    }
+}
