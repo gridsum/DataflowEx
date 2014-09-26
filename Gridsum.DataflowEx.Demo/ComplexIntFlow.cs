0ca111e7861575bf62789e58b857ae30c2d106d8
karldodd <karldodd@gmail.com>
2014-09-26 13:41:31 +0800

    Add Demo project

---
+++ Gridsum.DataflowEx.Demo/ComplexIntFlow.cs
@@ -0,0 +1,69 @@
+﻿namespace Gridsum.DataflowEx.Demo
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+    using Gridsum.DataflowEx;
+
+    public static class DataflowExEx
+    {
+        public static void Post<TIn>(this Dataflow<TIn> dataflow, TIn item)
+        {
+            dataflow.InputBlock.SafePost(item);
+        }
+
+        public static void RegisterChildren(this Dataflow dataflow, params Dataflow[] children)
+        {
+            foreach (var child in children)
+            {
+                dataflow.RegisterChild(child);
+            }
+        }
+    }
+
+    public class ComplexIntFlow : Dataflow<int>
+    {
+        private ITargetBlock<int> _headBlock;
+        public ComplexIntFlow() : base(DataflowOptions.Default)
+        {
+            Dataflow<int, int> node2 = DataflowUtils.FromDelegate<int, int>(i => i);
+            Dataflow<int, int> node3 = DataflowUtils.FromDelegate<int, int>(i => i * -1);
+
+            Dataflow<int, int> node1 = DataflowUtils.FromDelegate<int, int>(
+                i => {
+                        if (i % 2 == 0) { node2.Post(i); }
+                        else { node3.Post(i); }
+                        return 999;
+                    });
+            
+            Dataflow<int> printer = DataflowUtils.FromDelegate<int>(Console.WriteLine);
+
+            node1.Name = "node1";
+            node2.Name = "node2";
+            node3.Name = "node3";
+            printer.Name = "printer";
+
+            node1.LinkTo(printer);
+            node2.LinkTo(printer);
+            node3.LinkTo(printer);
+
+            //Completion propagation: node1 ---> node2
+            node2.RegisterDependency(node1);
+            //Completion propagation: node1 + node2 ---> node3
+            node3.RegisterDependency(node1);
+            node3.RegisterDependency(node2);
+
+            this.RegisterChild(node1);
+            this.RegisterChild(node2);
+            this.RegisterChild(node3);
+            this.RegisterChild(printer, t => { 
+                if (t.Status == TaskStatus.RanToCompletion) 
+                    Console.WriteLine("Printer done!");
+            });
+
+            this._headBlock = node1.InputBlock;
+        }
+
+        public override ITargetBlock<int> InputBlock { get { return this._headBlock; } }
+    }
+}
---
+++ Gridsum.DataflowEx.Demo/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Gridsum.DataflowEx.Demo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s =
+                "http://cn.bing.com/search?q=MD5CryptoServiceProvider+slow&qs=n&pq=md5cryptoserviceprovider+slow&sc=0-25&sp=-1&sk=&cvid=67d40cbd8c424d55a3db83e6e9868267&first=51&FORM=PERE4";
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] inBytes = Encoding.UTF8.GetBytes(s);
+                var bytes = md5.ComputeHash(inBytes);
+                Console.WriteLine(bytes.Length);
+            }
+
+
+            var splitter = new TransformBlock<string, KeyValuePair<string, int>>(
+                input =>
+                    {
+                        var splitted = input.Split('=');
+                        return new KeyValuePair<string, int>(splitted[0], int.Parse(splitted[1]));
+                    });
+
+            var dict = new Dictionary<string, int>();
+            var aggregater = new ActionBlock<KeyValuePair<string, int>>(
+                pair =>
+                    {
+                        int oldValue;
+                        dict[pair.Key] = dict.TryGetValue(pair.Key, out oldValue) ? oldValue + pair.Value : pair.Value;
+                    });
+
+            splitter.LinkTo(aggregater, new DataflowLinkOptions() { PropagateCompletion = true});
+
+            splitter.Post("a=1");
+            splitter.Post("b=2");
+            splitter.Post("a=5");
+
+            splitter.Complete();
+            aggregater.Completion.Wait();
+            Console.WriteLine("sum(a) = {0}", dict["a"]); //prints sum(a) = 6
+
+
+            CalcAsync().Wait();
+        }
+
+        public static async Task CalcAsync()
+        {
+            var aggregatorFlow = new AggregatorFlow();
+            aggregatorFlow.InputBlock.Post("a=1");
+            aggregatorFlow.InputBlock.Post("b=2");
+            aggregatorFlow.InputBlock.Post("a=5");
+            aggregatorFlow.InputBlock.Complete();
+            await aggregatorFlow.CompletionTask;
+            Console.WriteLine("sum(a) = {0}", aggregatorFlow.Result["a"]); //prints sum(a) = 6
+
+
+            //await aggregatorFlow.ProcessAsync(new[] { "a=1", "b=2", "a=5" }, completeFlowOnFinish: true);
+
+            var lineAggregator = new LineAggregatorFlow();
+            await lineAggregator.ProcessAsync(new[] { "a=1 b=2 a=5", "c=6 b=8" });
+            Console.WriteLine("sum(a) = {0}", lineAggregator["a"]); //prints sum(a) = 6
+
+
+            var intFlow = new ComplexIntFlow();
+            await intFlow.ProcessAsync(new[] { 1, 2, 3});
+        }
+    }
+}
