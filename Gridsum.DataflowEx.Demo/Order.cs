fcbe25de16b64df971166c7d99e00834299f9200
karldodd <karldodd@gmail.com>
2014-11-18 16:24:28 +0800

    add missing file

---
+++ Gridsum.DataflowEx.Demo/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gridsum.DataflowEx.Demo
+{
+    using Gridsum.DataflowEx.Databases;
+
+    public class Order
+    {
+        [DBColumnMapping("OrderTarget", "Date", null)]
+        public DateTime OrderDate { get; set; }
+
+        [DBColumnMapping("OrderTarget", "Value", 0.0f)]
+        public float? OrderValue { get; set; }
+
+        [NoNullCheck]
+        public Person Customer { get; set; }
+    }
+}
