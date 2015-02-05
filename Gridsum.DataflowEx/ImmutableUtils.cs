3303160bfd62385e21175143d118afec3d42920b
karldodd <karldodd@gmail.com>
2015-02-05 15:16:05 +0800

    Add TryAddManyOptimistically

--- Gridsum.DataflowEx/ImmutableUtils.cs
+++ Gridsum.DataflowEx/ImmutableUtils.cs
@@ -20,6 +20,8 @@ namespace Gridsum.DataflowEx
                 old = Volatile.Read(ref list);
                 added = old.Add(item);
                 beforeExchange = Interlocked.CompareExchange(ref list, added, old);
+
+                //todo: reuse beforeExchange for next old value
             }
             while (beforeExchange != old);
 
@@ -41,6 +43,38 @@ namespace Gridsum.DataflowEx
                 }
 
                 beforeExchange = Interlocked.CompareExchange(ref set, added, old);
+
+                //todo: reuse beforeExchange for next old value
+            }
+            while (beforeExchange != old);
+
+            return true;
+        }
+
+        public static bool TryAddManyOptimistically<T>(ref ImmutableHashSet<T> set, T[] items)
+        {
+            ImmutableHashSet<T> old, added;
+            ImmutableHashSet<T> beforeExchange;
+            do
+            {
+                old = Volatile.Read(ref set);
+
+                var builder = old.ToBuilder();
+                foreach (var item in items)
+                {
+                    builder.Add(item);
+                }
+
+                added = builder.ToImmutable();
+
+                if (object.ReferenceEquals(added, old))
+                {
+                    return false; //added nothing (all items exists)
+                }
+
+                beforeExchange = Interlocked.CompareExchange(ref set, added, old);
+
+                //todo: reuse beforeExchange for next old value
             }
             while (beforeExchange != old);
 
