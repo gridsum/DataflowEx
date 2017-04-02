1c1e8f92665b36e2d4b88ef2d2c7c42f05fb2681
hekaiduo <hekaiduo@gridsum.com>
2017-04-02 23:52:08 +0800

    Add TestBootstrapper for docker init (not working yet)

---
+++ Gridsum.DataflowEx.Test/DatabaseTests/TestBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using System.Threading;
+
+namespace Gridsum.DataflowEx.Test.DatabaseTests
+{
+    [TestClass]
+    public class TestBootstrapper
+    {
+        [AssemblyInitialize]
+        public static void BootSqlServerOnLinuxWithDocker(TestContext tc)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("tcp://localhost:2373")).CreateClient();
+            client.Containers.StartContainerAsync("microsoft/mssql-server-linux", new ContainerStartParameters { }).Wait();
+        }
+
+        [AssemblyCleanup]
+        public static void ShutdownSqlServerDockerImage()
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("tcp://localhost:2373")).CreateClient();
+            client.Containers.StopContainerAsync("microsoft/mssql-server-linux", new ContainerStopParameters { }, new CancellationToken()).Wait();
+        }
+    }
+}
